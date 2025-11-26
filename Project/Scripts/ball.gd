d459964a246c8175353ed1be5dc8dc422577d130
Dee <dee.covan@gmail.com>
2025-11-26 12:01:01 +0200

    Add Ball is_lowspeed and add_friction functions

--- Project/Scripts/ball.gd
+++ Project/Scripts/ball.gd
@@ -1,15 +1,29 @@
 extends RigidBody3D
 
-signal stopped
+var lowspeed = 1.0
+var low_crit = 0.1
 
-func shoot(angle, power):
-	var force = Vector3.FORWARD.rotated(Vector3.UP, angle)
-	apply_central_impulse(force * power)
+signal stopped
 	
 func _integrate_forces(state):
-	if state.linear_velocity.length() < 0.1:
+	if (is_lowspeed(state.linear_velocity)):
+		state.linear_velocity = add_friction(state.linear_velocity)
+	if state.linear_velocity.length() < 0.2:
 		stopped.emit()
 		state.linear_velocity = Vector3.ZERO
 	# Ball fell off course!
 	if position.y < -20:
 		get_tree().reload_current_scene()
+		
+## Functions
+
+## Check the Speed is Low and verical is 0
+func is_lowspeed(speed: Vector3) -> bool:
+	return speed.length() < lowspeed and speed.y < low_crit
+
+## Add LERP friction simulation for the ball at low speeds
+func add_friction(speed: Vector3) -> Vector3:
+	speed[0] = lerp(speed[0], 0.0, 0.1)
+	speed[1] = lerp(speed[1], 0.0, 0.1)
+	speed[2] = lerp(speed[2], 0.0, 0.1)
+	return speed
