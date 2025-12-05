e07f3c7befadfe1f9e4376e32dc92b7bc4d3c751
Dee <dee.covan@gmail.com>
2025-12-05 21:47:37 +0200

    Update Terra_Sand materials and shader

--- Project/Scripts/terra_collision_gen.gd
+++ Project/Scripts/terra_collision_gen.gd
@@ -3,7 +3,7 @@ extends MeshInstance3D
 @export var noised = 1.0/40
 
 func _ready():
-		var cs = $StaticBody3D/CollisionShape3D
+		var cs = $CollisionShape3D2
 		var i = 0
 		for v in cs.shape.map_data:
 			cs.shape.map_data[i] = (
