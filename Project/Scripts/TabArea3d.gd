2b601adfb61cc0f4144e1c12c86d0f63f2ed0b98
Dee <dee.covan@gmail.com>
2025-12-10 16:00:54 +0200

    Add .import to git

--- Project/Scripts/TabArea3d.gd
+++ Project/Scripts/TabArea3d.gd
@@ -9,10 +9,11 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is VehicleBody3D:
-		lap_timer_node.text == lap_timer
+		lap_timer_node.text == var_to_str(lap_timer)
 		lap_timer_started = true
 		lap_timer = 0.0
 			
 func _physics_process(delta: float) -> void:
 	if lap_timer_started:
 		lap_timer += delta
+		lap_timer_node.text == var_to_str(lap_timer)
